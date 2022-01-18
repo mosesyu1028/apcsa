ae9f83483a7efb616f24dc3444afc10def7997a3
yu_0901 <yu_0901@school.ics.edu.hk>
2022-01-18 11:26:08 +0800

    prepare 7-3 lesson

---
+++ 7-3/Arrays2D.java
@@ -0,0 +1,32 @@
+public class Arrays2D {
+    public static void main(String args[]) {
+
+        // Store this grid in a variable:
+        // 2 6 8 7
+        // 1 5 4 0
+        // 9 3 2 8
+
+
+
+
+        // mat[0]  contains  {2, 6, 8, 7}
+        // mat[1]  contains  {1, 5, 4, 0}    
+        // mat[2]  contains  {9, 3, 2, 8}
+
+        // mat.length = ?
+        // mat[0].length = ?
+
+        // mat[r][c] = ?
+        
+        // Ranges:
+        // rows: 0 to mat.length - 1
+        // columns: 0 to mat[0].length - 1
+        // be careful of ArrayIndexOutOFBoundsException
+
+        // common ways to traverse 2d arrays:
+        // 1. row-column (rw)
+        // 2. enhanced for loop (r)
+        // 3. row-by-row processing (rwx)
+
+    }
+}
